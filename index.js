e8986cb2775a10a078d6f2d9be24874a90802f76
kotaewing <kota.ewing@gmail.com>
2021-05-15 14:25:14 -0600

    added options that succesfully route to engineer and intern functions

--- index.js
+++ index.js
@@ -6,6 +6,68 @@ const generateTemplate = require('./src/htmlTemplate');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// prompt for team managers info
+class websiteCreation {
 
+    teamManager() {
+        inquirer
+        .prompt([
+            {
+                type: 'text',
+                name: 'name',
+                message: "Team manager's name: "
+            },
+            {
+                type: 'text',
+                name: 'id',
+                message: "Team manager's employee id: "
+            },
+            {
+                type: 'text',
+                name: 'email',
+                message: "Team manager's email: "
+            },
+            {
+                type: 'text',
+                name: 'officeNum',
+                message: "Team manager's office number: "
+            }
+        ])
+        .then(() => {
+            this.selectOptions()
+        })
+    }
+    
+    // options to add engineer or intern or complete team
+    selectOptions() {
+        inquirer
+        .prompt({
+            type: 'list',
+            name: 'options',
+            message: 'Choose an option below: ',
+            choices: ['Add New Engineer', 'Add New Intern', 'Complete Your Team']
+        })
+        .then(({ options }) => {
+            if (options === 'Add New Engineer') {
+                this.addEngineer();
+            } else if (options === 'Add New Intern') {
+                this.addIntern();
+            } else {
+                console.log('completed')
+            }
+        })
+    }
+    
+    // prompt for engineer info
+    addEngineer() {
+        console.log('engineer');
+    }
+    // prompt for intern info
+    addIntern() {
+        console.log('intern')
+    }
+}
+
+new websiteCreation().teamManager();
 
 
