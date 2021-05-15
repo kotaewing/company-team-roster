27c1b775038259ecbd40389ae086f1d2118814a4
kotaewing <kota.ewing@gmail.com>
2021-05-15 12:04:59 -0600

    manager object generates with inherited attributes from employee object and new office number

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@jest/globals');
+const Manager = require('../lib/Manager');
+
+test('should generate a manager with name, email, and id', () => {
+    const manager = new Manager('Dakota', 20, 'example@yahoo.com');
+
+    expect(manager.name).toBe('Dakota')
+    expect(manager.id).toBe(20)
+    expect(manager.email).toBe('example@yahoo.com');
+});
+
+test('should generate a manager with an office number', () => {
+    const manager = new Manager('Dakota', 20, 'example@yahoo.com', 5);
+
+    expect(manager.officeNum).toBe(5);
+})
+
+test('should return role as manager', () => {
+    const manager = new Manager('Dakota', 20, 'example@yahoo.com', 5);
+
+    expect(manager.getRole()).toBe('Manager');
+})
