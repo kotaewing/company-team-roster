923dd3bd214a7065a9ca9ac296b0809b0698f3c3
kotaewing <kota.ewing@gmail.com>
2021-05-15 12:16:57 -0600

    engineer now has github attached

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
 
 test('should generate engineer with name, email, and id', () => {
@@ -7,3 +8,16 @@ test('should generate engineer with name, email, and id', () => {
     expect(engineer.id).toBe(20)
     expect(engineer.email).toBe('example@yahoo.com');
 })
+
+test('should generate engineer with github', () => {
+    const engineer = new Engineer('Dakota', 20, 'example@yahoo.com', 'kotaewing');
+
+    expect(engineer.github).toBe('kotaewing')
+})
+
+test('should return engineer github', () => {
+    const engineer = new Engineer('Dakota', 20, 'example@yahoo.com', 'kotaewing');
+
+    expect(engineer.getGithub()).toBe('kotaewing');
+})
+
